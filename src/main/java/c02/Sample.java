a3d17072402328786503e90e0e562827be60bf21
OsamuGoto <osagoto@sea.plala.or.jp>
2025-10-03 14:18:44 +0900

    新規作成

---
+++ src/main/java/c02/Sample.java
@@ -0,0 +1,25 @@
+package c02;
+
+import java.io.IOException;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Sample
+ */
+@WebServlet("/Sample")
+public class Sample extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+}
